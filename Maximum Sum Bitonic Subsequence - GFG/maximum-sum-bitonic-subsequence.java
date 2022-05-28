05aae07ec8834cfe1a2ea193619cc07264966246
Abhishek_gbpec2019 <69711362+mishraabhi7074@users.noreply.github.com>
2022-05-28 20:39:42 +0530

    Added solution - LeetHub

---
+++ Maximum Sum Bitonic Subsequence - GFG/maximum-sum-bitonic-subsequence.java
@@ -0,0 +1,86 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            System.out.println(obj.maxSumBS(a, n));
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Compute {
+    
+    public static int LIS_SumDP(int[] arr, int[] dp) {
+        int maxSum = 0, n = arr.length;
+        for (int i = 0; i < n; i++) {
+            dp[i] = arr[i];
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
+            }
+
+            maxSum = Math.max(maxSum, dp[i]);
+        }
+
+        return maxSum;
+    }
+
+    public static int LDS_SumDP(int[] arr, int[] dp) {
+        int maxSum = 0, n = arr.length;
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = arr[i];
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
+            }
+
+            maxSum = Math.max(maxSum, dp[i]);
+        }
+
+        return maxSum;
+    }
+    
+     public static int maxSumBS(int arr[], int n) {
+
+        int[] LIS = new int[n];
+        int[] LDS = new int[n];
+
+        LIS_SumDP(arr, LIS);
+        LDS_SumDP(arr, LDS);
+
+        int maxSum = 0;
+        for (int i = 0; i < n; i++) {
+            maxSum = Math.max(maxSum, LIS[i] + LDS[i] - arr[i]);
+        }
+        return maxSum;
+
+    }
+}
