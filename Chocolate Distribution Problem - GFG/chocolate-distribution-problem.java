2f96e55869ac2852947223fddaa03b993e73dbdb
Abhishek_gbpec2019 <69711362+mishraabhi7074@users.noreply.github.com>
2022-05-31 11:14:25 +0530

    Added solution - LeetHub

---
+++ Chocolate Distribution Problem - GFG/chocolate-distribution-problem.java
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            ArrayList<Integer> arr = new ArrayList<>();
+            for(int i = 0;i<n;i++)
+                {
+                    int x = sc.nextInt();
+                    arr.add(x);
+                }
+            int m = sc.nextInt();
+            
+            Solution ob = new Solution();
+            
+    		System.out.println(ob.findMinDiff(arr,n,m));
+        }
+        
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+        // your code here
+        Collections.sort(a);
+        if(n ==0 || m==0){
+            return 0;
+        }
+        if(n < m){
+            return -1;
+        }
+        int mindiff = Integer.MAX_VALUE;
+        for(int i =0; i + m -1 < a.size(); i++){
+            int diff = a.get(i + m -1) - a.get(i);
+            if(diff < mindiff){
+                mindiff = diff;
+            }
+            
+        }
+        return mindiff;
+    }
+}
