ffbe16a7f5049530c396b87184fac2322671f932
Abhishek_gbpec2019 <69711362+mishraabhi7074@users.noreply.github.com>
2022-06-09 19:15:44 +0530

    Time: 18 ms (28.23%), Space: 44.3 MB (62.58%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int lengthOfLongestSubstring(String str) {
+        int ans =0;
+        int i = -1;
+        int j = -1;
+        HashMap<Character, Integer> map = new HashMap<>();
+        while(true){
+            boolean f1 = false;
+            boolean f2 = false;
+        
+        
+            // acquire
+            while(i < str.length() -1){
+                f1 = true;
+                i++;
+                char ch = str.charAt(i);
+                map.put(ch, map.getOrDefault(ch,0) + 1);
+                
+                if(map.get(ch) == 2){
+                    break;
+                    
+                }else{
+                    int len = i -j;
+                    if(len > ans){
+                        ans = len;
+                    }
+                }
+            }
+            
+            // release
+            while(j < i){
+                f2 = true;
+                j++;
+                char ch = str.charAt(j);
+                map.put(ch, map.get(ch) -1);
+                if(map.get(ch) == 1){
+                    break;
+                }
+            }
+            if(f1 == false && f2 == false){
+                break;
+            }
+        }
+             
+        return ans;
+    }
+}
