701d46680c7464f1afec934f672579a0d95bde37
Abhishek_gbpec2019 <69711362+mishraabhi7074@users.noreply.github.com>
2022-06-15 14:23:46 +0530

    Time: 4 ms (70.74%), Space: 58.5 MB (7.19%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,26 +1,31 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int lands = 0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j] == '1'){
-                    lands++;
-                    DFS(grid,i,j);
-                }
+        int count =0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for(int i=0; i < grid.length; i++){
+            for(int j =0; j < grid[0].length; j++){
+               if(visited[i][j] == false && grid[i][j] == '1'){
+                   count++;
+                   traversal(grid, visited, i, j);
+                   
+               }   
             }
         }
-        return lands;
+        return count;
     }
-    int[][] dirs = {{0,1}, {1,0}, {0, -1}, {-1, 0}};
-    public void DFS(char[][] grid,int row, int col){
-        if (row < 0 || row >= grid.length || col < 0 || col >=grid[0].length || grid[row][col] == '0'){ 
-           return;
+    
+    public void traversal(char[][] grid, boolean[][] visited, int i, int j){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length){
+            return;
+        }else if(visited[i][j] == true){
+            return;
+        }else if(grid[i][j] == '0'){
+            return;
         }
-        grid [row][col] = '0';
-        for(int [] curr:dirs){
-            int x = row+curr[0];
-            int y = col+curr[1];
-            DFS(grid,x,y);
-        }
+        visited[i][j] = true;
+        traversal(grid, visited, i-1, j);
+        traversal(grid, visited, i+1, j);
+        traversal(grid, visited, i, j-1);
+        traversal(grid, visited, i, j+1);
     }
 }
