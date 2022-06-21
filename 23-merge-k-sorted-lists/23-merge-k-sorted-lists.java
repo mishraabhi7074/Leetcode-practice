a39180c33758f6995a990db0818febbb8dec5234
Abhishek_gbpec2019 <69711362+mishraabhi7074@users.noreply.github.com>
2022-06-22 02:37:59 +0530

    Time: 3 ms (90.00%), Space: 46.9 MB (70.00%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -9,31 +9,41 @@
  * }
  */
 class Solution {
-    public static ListNode mergeKLists(ListNode[] lists){
-    return partion(lists,0,lists.length-1);
-}
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null || l2 == null) return l1 != null ? l1:l2;
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
 
-public static ListNode partion(ListNode[] lists,int s,int e){
-    if(s==e)  return lists[s];
-    if(s<e){
-        int q=(s+e)/2;
-        ListNode l1=partion(lists,s,q);
-        ListNode l2=partion(lists,q+1,e);
-        return merge(l1,l2);
-    }else
-        return null;
-}
+        ListNode c1 = l1;
+        ListNode c2 = l2;
+        while(c1 != null && c2 != null){
+            if(c1.val < c2.val){
+                prev.next = c1;
+                c1 = c1.next;
+            }else{
+                prev.next = c2;
+                c2 = c2.next;
+            }
 
-//This function is from Merge Two Sorted Lists.
-public static ListNode merge(ListNode l1,ListNode l2){
-    if(l1==null) return l2;
-    if(l2==null) return l1;
-    if(l1.val<l2.val){
-        l1.next=merge(l1.next,l2);
-        return l1;
-    }else{
-        l2.next=merge(l1,l2.next);
-        return l2;
+            prev = prev.next;
+        }
+        prev.next = c1 != null ? c1:c2;
+        return dummy.next;
     }
-}
+    
+     public static ListNode mergeKLists(ListNode[] lists, int si, int ei) {
+        if(si > ei) return null;
+        if(si == ei) return lists[si];
+        int mid = (si + ei)/2;
+
+        ListNode l1= mergeKLists(lists, si, mid);
+        ListNode l2= mergeKLists(lists, mid+1, ei);
+
+        return mergeTwoLists(l1, l2);
+    }
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0) return null;
+        return mergeKLists(lists,0, lists.length -1);
+    }
 }
