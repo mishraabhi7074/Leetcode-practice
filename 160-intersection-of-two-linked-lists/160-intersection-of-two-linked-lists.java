41485823618ece281eb1e4551cab3f0a06ba5718
Abhishek_gbpec2019 <69711362+mishraabhi7074@users.noreply.github.com>
2022-06-24 02:05:50 +0530

    Time: 2 ms (53.98%), Space: 55.9 MB (17.99%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -11,34 +11,32 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        // using two pointers technique we can solve this
-        // given 2 linkedlists and there are some nodes that are repeated in both lists
-        // if we calculate sum of both the linkedliists size will be some n
-        // if we start 1 pointer at 1st list and 2nd one at 2nd list and when it reaches tail, reference it to other lists head and they'll meet at the intersection node; 
-        // 
-        ListNode ptr1 = headA;
-        ListNode ptr2 = headB;
-        
-        while(ptr1!=ptr2){
-            // increment both the pointers
-            ptr1 = ptr1.next;
-            ptr2 = ptr2.next;
-            
-            if(ptr1==ptr2){
-                return ptr1;
-            }
-            
-            // if ptr1 --> headA reaches tail, reference it to headB
-            if(ptr1==null){
-                ptr1 = headB;
-            }
-            
-            // if ptr2 --> headB reaches tail, reference it to headA
-            if(ptr2==null){
-                ptr2 =headA;
-            }
-        }
-        
-        return ptr1;
+        if(headA == null || headB == null) return null;
+        ListNode tail = headA;
+        while(tail.next != null) tail = tail.next;
+        tail.next = headB;
+        ListNode ans = detectCycle(headA);
+        tail.next = null;
+        return ans;
     }
+    
+     public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == fast) break;
+        }
+        if(slow != fast) return null;
+
+            slow = head;
+            fast = fast;
+            while(slow != fast){
+                slow = slow.next;
+                fast = fast.next;
+            }
+        return slow;
+     }  
 }
