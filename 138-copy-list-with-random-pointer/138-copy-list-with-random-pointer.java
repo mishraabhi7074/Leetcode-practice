5ac375fdb01df146f7081a52203835e95e3ecb96
Abhishek_gbpec2019 <69711362+mishraabhi7074@users.noreply.github.com>
2022-06-23 19:55:13 +0530

    Time: 0 ms (100.00%), Space: 45.5 MB (49.18%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -15,46 +15,46 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        if(head == null){
-           return null;
-        }
-       
-        //step--1
-        Node p = head;
-        while(p!= null){
-            Node n = new Node(p.val);
-            n.next = p.next;
-            p.next = n;
-            p = n.next;
-        }
-        
-        //step--2
-        p = head;
-        while(p!= null){
-            if(p.random!= null){
-                 p.next.random = p.random.next;
+        copylist(head);
+        copyrandompointer(head);
+        return extractdeepcopy(head);
+    }
+    
+    //copy random pointer
+    public static void copyrandompointer(Node head){
+        Node curr = head;
+        while(curr != null){
+            Node random = curr.random;
+            if(random != null){
+                curr.next.random = random.next;
             }
-           
-            p = p.next.next;
+            curr = curr.next.next;
         }
-        
-        //step--3
-        
-        p = head;
-        Node head1 = head.next;
-        Node q = head1;
-        while(q.next != null){
-            
-            p.next = q.next;
-            p = p.next;
-            
-            q.next = q.next.next;
-            q = q.next;
-            
+    }
+    //extract deep copy
+    public static Node extractdeepcopy(Node head){
+        Node dummy = new Node(-1);
+        Node prev = dummy, curr = head;
+
+        while(curr != null){
+            prev.next = curr.next;
+            curr.next = curr.next.next;
+
+            prev = prev.next;
+            curr = curr.next;
         }
-        p.next = null;
-        
-        return head1;
-        
+        return dummy.next;
+    }
+    
+     //copy list
+    public static void copylist(Node head){
+        Node curr = head;
+        while(curr != null){
+            Node forw = curr.next;
+            Node node = new Node(curr.val);
+            curr.next = node;
+            node.next = forw;
+            curr = forw;
+        }
     }
 }
