f10bd33929aebe90982e41845a6596e3622c6ed3
Abhishek_gbpec2019 <69711362+mishraabhi7074@users.noreply.github.com>
2022-08-04 12:25:09 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (76.41%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -13,37 +13,39 @@
  *     }
  * }
  */
-/*  if node have camera -> return 0;
-    if node need camera -> return -1;
-    if node is covered -> return 1;
-    
-    and we will go in postOrder
-*/
 class Solution {
-    int cameras = 0;
     
-    int solve(TreeNode root){      // postorder traversal
-        if(root == null)   
-            return 1;   //no need of camera node is covered
+    int count = 0;
+    final int CAMERA = 1;
+    final int NOT_MONITORED = 2;
+    final int MONITORED = 3;
+    public int minCameraCover(TreeNode root) {
+        int val = helper(root);
         
-        int left = solve(root.left);
-        int right = solve(root.right);
-        
-        if(left == -1 || right == -1){   //then we have to give a camera
-            cameras++;
-            return 0;        // current node have camera
-        }
-        else if(left == 0 || right == 0)
-            return 1;      // already covered by children
-        else              //childrens are covered and non of the children have camera. so to cover current node its parent will handle
-            return -1;     //current node need camera
+        if(val == NOT_MONITORED){
+         count++;
+     }
+        return count;
         
     }
     
-    public int minCameraCover(TreeNode root) {
-        if(solve(root) == -1)   // if root need camera
-            cameras++;
+    public int helper(TreeNode node){
+        if(node == null){
+            return MONITORED;
+        }
         
-        return cameras;
+        int left = helper(node.left);
+        int right = helper(node.right);
+        
+        if(left == NOT_MONITORED || right == NOT_MONITORED){
+            count++;
+            return CAMERA;
+            
+        }else if(left == CAMERA || right == CAMERA){
+            return MONITORED;
+        }else{
+            return NOT_MONITORED;
+        }
+        
     }
 }
